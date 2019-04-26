16a711e8d83a38b25c4f51b66c275362443ef1f3
ctnilpp <782237557@qq.com>
2019-04-26 15:34:20 +0800

    高精度

--- 高精度/高精度乘法.cpp
+++ 高精度/高精度乘法.cpp
@@ -1,8 +1,30 @@
 #include<iostream>
-#include<algorithm>
-#include<cmath>
+#include<cstring>
 using namespace std;
-int main(){
-	
-	return 0;
-} 
+char a1[100],b1[100];
+int a[100],b[100],c[300],lena,lenb,lenc,i,j;
+int main()
+{
+    gets(a1);
+    gets(b1);
+    lena = strlen(a1);
+    lenb = strlen(b1);
+    cout<<"lena"<<lena<<endl;
+    cout<<"lenb"<<lenb<<endl;
+    for(int i=0;i<lena;i++) a[lena-i] = a1[i]-48;
+    for(int i=0;i<lenb;i++) b[lenb-i] = b1[i]-48;
+    for(int i=1;i<=lena;i++){
+    	for(int j=1;j<=lenb;j++){
+    		     c[i+j-1] = c[i+j-1]+a[i]*b[j]+c[i+j-2]/10;
+				 c[i+j-2]%=10;    
+		}
+	}
+	lenc = lena+lenb;
+		 while(c[lenc]==0&&lenc>1)
+		 	 lenc--;
+	for(int i=lenc;i>=1;i--){
+		 cout<<c[i];
+	}
+	cout<<endl;
+    return 0;
+}
