774707040e1e0e47d6ae62bf5ae91bcc580044d0
ctnilpp <49940531+ctnilpp@users.noreply.github.com>
2020-03-19 12:51:42 +0800

    p1001
    p1001AC

---
+++ zipeng/src/p1001/Main.java
@@ -0,0 +1,15 @@
+package p1001;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt();
+		int b = cin.nextInt();
+		System.out.println(a+b);
+	}
+
+}
