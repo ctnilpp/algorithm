706313b17f5ed9195cfdeb6643c54a79b9f027ca
ctnilpp <49940531+ctnilpp@users.noreply.github.com>
2020-03-30 16:40:40 +0800

    add
    p1005
    p1007
    p1011
    p1015

---
+++ zipeng/src/p1005/Main.java
@@ -0,0 +1,15 @@
+package p1005;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner cin = new Scanner(System.in);
+		int n = cin.nextInt();
+		int m = cin.nextInt();
+		
+	}
+
+}
---
+++ zipeng/src/p1015/Main.java
@@ -0,0 +1,14 @@
+package p1015;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner cin = new Scanner(System.in);
+		int N = cin.nextInt();
+		int M = cin.nextInt();
+	}
+
+}
