e98c041973acb62a75e544515004e1df0a19d91b
ctnilpp <49940531+ctnilpp@users.noreply.github.com>
2020-04-01 10:41:49 +0800

    add
    p1161
    p2911
    p5731
    p1161XOR

--- zipeng/src/p2911/Main.java
+++ zipeng/src/p2911/Main.java
@@ -1,10 +1,32 @@
 package p2911;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner cin = new Scanner(System.in);
+		int sum[] = new int[100];
+		int s1 = cin.nextInt();
+		int s2 = cin.nextInt();
+		int s3 = cin.nextInt();
+		int max = 0;
+		int index =0;
+		for(int i=1;i<=s1;i++){
+			for(int j=1;j<=s2;j++){
+				for(int k=1;k<=s3;k++){
+					sum[i+j+k]++;
+				}
+			}
+		}
+		for(int i=3;i<=s1+s2+s3;i++){
+			 if(sum[i]>max){
+				 max=sum[i];
+				 index = i;
+			 }
+		}
+		System.out.println(index);
 	}
 
 }
